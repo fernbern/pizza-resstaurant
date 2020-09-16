e2ac76e7fb295607160d914af03993d763dac850
fernbern <70728008+fernbern@users.noreply.github.com>
2020-09-16 16:55:40 -0700

    About Finished

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function lapizzeria_setup() {
     // add thumbnails
     add_theme_support('post-thumbnails');
+    add_image_size('boxes', 437, 291, true);
 }
 
 add_action('after_setup_theme', 'lapizzeria_setup');
