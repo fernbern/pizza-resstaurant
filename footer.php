0bb7558e05223fc4160f6ba1319156cccd4860c2
fernbern <70728008+fernbern@users.noreply.github.com>
2020-09-17 08:01:39 -0700

    start menu page

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
+<footer>
+    <?php
+        $args = array(
+            'theme_location' => 'header-menu',
+            'container' => 'nav',
+            'after' => '<span class="separator"> | </span>'
+        );
+
+        wp_nav_menu($args);
+     ?>
+
+     <div class="location">
+     <p>2754 Ocean Avenue Pallo Alto, CA 90481</p>
+     <p>Phone: 777-777-7777</p>
+     </div>
+     <p class="copyright">All Rights Reserved <?php echo date('Y'); ?></p>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
