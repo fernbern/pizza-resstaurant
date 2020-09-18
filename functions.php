d3b53f09f6f1c0401dad46f9e0c02034543bf655
fernbern <70728008+fernbern@users.noreply.github.com>
2020-09-17 18:58:11 -0700

    Almost Done

--- functions.php
+++ functions.php
@@ -77,3 +77,22 @@ function lapizzeria_specialties() {
 }
 
 add_action( 'init', 'lapizzeria_specialties' );
+
+/**
+ * WIDGETS ZONE
+ */
+
+ function lapizzeria_widgets() {
+	 register_sidebar(
+		 array(
+			'name' => 'Blog Sidebar',
+			'id' => 'blog_sidebar',
+			'before_widget' => '<div class="widget">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>'
+		 )
+		 );
+ }
+
+ add_action('widgets_init', 'lapizzeria_widgets');
